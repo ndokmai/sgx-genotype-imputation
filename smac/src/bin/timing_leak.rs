96c540a9502e05b41afd8cdb2e72fa880cb08b26
Ko Dokmai <natnatee.dokmai@gmail.com>
2021-03-16 11:38:59 -0400

    Fix measurement code in timing leak

--- smac/src/bin/timing_leak.rs
+++ smac/src/bin/timing_leak.rs
@@ -9,119 +9,46 @@ use std::hint::black_box;
 type F = TpLnFixed<typenum::U20>;
 
 fn main() {
-    let n = 10000;
-    let n_fold = 1000;
+    let args = std::env::args().collect::<Vec<String>>();
+    let save = if args.len() == 2 {
+        if args[1] == "--save-results" {
+            true
+        } else {
+            false
+        }
+    } else {
+        false
+    };
+
+    let n = 100000;
+    let n_fold = 100;
     let alpha = 0.05;
-    if_else_tests(n, alpha, n_fold);
-    f32_tests(n, alpha, n_fold);
-    f64_tests(n, alpha, n_fold);
-    const_select_tests(n, alpha, n_fold);
-    fixed_tests(n, alpha, n_fold);
+    if_else_tests(n, n_fold, alpha, save);
+    f32_tests(n, n_fold, alpha, save);
+    const_select_tests(n, n_fold, alpha, save);
+    fixed_tests(n, n_fold, alpha, save);
 }
 
 fn save_results(baseline: Vec<f64>, test: Vec<f64>, title: &str) {
     use std::fs::File;
     use std::io::Write;
+    std::fs::create_dir_all("results").unwrap();
     let f_baseline_name = format!("results/{}_baseline.txt", title);
     let mut f_baseline = File::create(f_baseline_name).unwrap();
     baseline
         .into_iter()
-        .for_each(|v| writeln!(&mut f_baseline, "{}", v).unwrap());
+        .for_each(|v| writeln!(&mut f_baseline, "{:.5}", v).unwrap());
     let f_test_name = format!("results/{}_test.txt", title);
     let mut f_test = File::create(f_test_name).unwrap();
     test.into_iter()
-        .for_each(|v| writeln!(&mut f_test, "{}", v).unwrap());
+        .for_each(|v| writeln!(&mut f_test, "{:.5}", v).unwrap());
 }
 
-fn if_else_tests(n: usize, alpha: f64, n_fold: usize) {
+fn if_else_tests(n: usize, n_fold: usize, alpha: f64, save: bool) {
     let title = "if-else";
     let title = format!("===== {} z-test =====", title);
     println!("{}", title.bold());
     println!("N = {}; alpha = {}", n, alpha);
-    if_else_template(n, n_fold, alpha);
-    println!("");
-    println!("");
-}
-
-macro_rules! op {
-    ($o: tt, $n_fold: expr) => {
-    |x, y| (0..$n_fold).fold(x, |_, _| black_box(black_box(x) $o black_box(y)))
-    }
-}
-
-fn f32_tests(n: usize, alpha: f64, n_fold: usize) {
-    let title = "f32";
-    let a = (1f32, "1.0");
-    let b = (1f32, "1.0");
-    let c = (1e-38f32, "VERY_SMALL");
-
-    let f = op! {+, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "+");
-
-    let f = op! {-, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "-");
-
-    let f = op! {*, n_fold};
-    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "*");
-    save_results(baseline, test, format!("{}_{}", title, "mult").as_str());
-
-    let f = op! {/, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "/");
-}
-
-fn f64_tests(n: usize, alpha: f64, n_fold: usize) {
-    let title = "f64";
-    let a = (1f64, "1.0");
-    let b = (1f64, "1.0");
-    let c = (1e-320f64, "VERY_SMALL");
-
-    let f = op! {+, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "+");
-
-    let f = op! {-, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "-");
-
-    let f = op! {*, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "*");
-
-    let f = op! {/, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "/");
-}
-
-fn const_select_tests(n: usize, alpha: f64, n_fold: usize) {
-    let title = "fixed-select";
-    let title = format!("===== {} z-test =====", title);
-    println!("{}", title.bold());
-    println!("N = {}; alpha = {}", n, alpha);
-    const_select_template(true, false, n, n_fold, alpha);
-    const_select_template(false, true, n, n_fold, alpha);
-    let (baseline, test) = const_select_template(false, false, n, n_fold, alpha);
-    save_results(baseline, test, "fixed-select");
-    println!("");
-    println!("");
-}
-
-fn fixed_tests(n: usize, alpha: f64, n_fold: usize) {
-    let title = "fixed-time-ln";
-    let a = (F::ONE, "ln(0.0)");
-    let b = (F::ONE, "ln(0.0)");
-    let c = (F::NAN, "ln(VERY_LARGE)");
-
-    let f = op! {+, n_fold};
-    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "+");
-    save_results(baseline, test, format!("{}_{}", title, "add").as_str());
-
-    let f = op! {-, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "-");
-
-    let f = op! {*, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "*");
-
-    let f = op! {/, n_fold};
-    op_template(n, n_fold, alpha, a, b, c, f, title, "/");
-}
-
-fn if_else_template(n: usize, n_fold: usize, alpha: f64) {
     println!("-------------------");
     let x_str = format!("time(if {{ EXPENSIVE }})");
     let u_str = format!("time(else {{ CHEAP }})");
@@ -129,12 +56,16 @@ fn if_else_template(n: usize, n_fold: usize, alpha: f64) {
     println!("H_1: {} != {}", x_str, u_str);
 
     let f = |cond| {
-        (0..n_fold).fold(1.0, |_, _| {
-            black_box(if black_box(cond) {
-                black_box(1.0) * black_box(1e-38f32)
-            } else {
-                black_box(1.0) * black_box(1.0)
-            })
+        black_box({
+            let mut z = 0.0;
+            for _ in 0..n_fold {
+                black_box(if black_box(cond) {
+                    z = black_box(black_box(1.0) * black_box(1e-38f32));
+                } else {
+                    z = black_box(black_box(1.0) * black_box(1.0));
+                })
+            }
+            z
         });
     };
 
@@ -142,7 +73,103 @@ fn if_else_template(n: usize, n_fold: usize, alpha: f64) {
     let f_test = || f(false);
     let (baseline, test) = time_measure_all(n, n_fold, f_baseline, f_test);
     let (baseline, test) = ztest(baseline, test, alpha, &x_str, &u_str);
-    save_results(baseline, test, "if-else");
+    if save {
+        save_results(baseline, test, "if-else");
+    }
+    println!("");
+    println!("");
+}
+
+macro_rules! op {
+    ($o: tt, $n_fold: expr) => {
+        |x, y| {
+            black_box({
+                let mut z = x;
+                for _ in 0..$n_fold {
+                    z = black_box(black_box(x) $o black_box(y))
+                };
+                z
+            }
+            )
+        }
+    }
+}
+
+fn f32_tests(n: usize, n_fold: usize, alpha: f64, save: bool) {
+    let title = "f32";
+    let a = (1f32, "1.0");
+    let b = (1f32, "1.0");
+    let c = (1e-38f32, "VERY_SMALL");
+
+    let f = op! {+, n_fold};
+    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "+");
+    if save {
+        save_results(baseline, test, format!("{}_{}", title, "add").as_str());
+    }
+
+    let f = op! {-, n_fold};
+    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "-");
+    if save {
+        save_results(baseline, test, format!("{}_{}", title, "sub").as_str());
+    }
+
+    let f = op! {*, n_fold};
+    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "*");
+    if save {
+        save_results(baseline, test, format!("{}_{}", title, "mult").as_str());
+    }
+
+    let f = op! {/, n_fold};
+    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "/");
+    if save {
+        save_results(baseline, test, format!("{}_{}", title, "div").as_str());
+    }
+}
+
+fn const_select_tests(n: usize, n_fold: usize, alpha: f64, save: bool) {
+    let title = "fixed-select";
+    let title = format!("===== {} z-test =====", title);
+    println!("{}", title.bold());
+    println!("N = {}; alpha = {}", n, alpha);
+    const_select_template(true, false, n, n_fold, alpha);
+    const_select_template(false, true, n, n_fold, alpha);
+    let (baseline, test) = const_select_template(false, false, n, n_fold, alpha);
+    if save {
+        save_results(baseline, test, "fixed-select");
+    }
+    println!("");
+    println!("");
+}
+
+fn fixed_tests(n: usize, n_fold: usize, alpha: f64, save: bool) {
+    let title = "fixed-time-ln";
+    let a = (F::ONE, "ln(0.0)");
+    let b = (F::ONE, "ln(0.0)");
+    let c = (F::NAN, "ln(VERY_LARGE)");
+
+    let f = op! {+, n_fold};
+    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "+");
+    if save {
+        save_results(baseline, test, format!("{}_{}", title, "add").as_str());
+    }
+
+    let f = op! {-, n_fold};
+    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "-");
+    if save {
+        save_results(baseline, test, format!("{}_{}", title, "sub").as_str());
+    }
+
+    let f = op! {*, n_fold};
+    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "*");
+    if save {
+        save_results(baseline, test, format!("{}_{}", title, "mult").as_str());
+    }
+
+    let f = op! {/, n_fold};
+    let (baseline, test) = op_template(n, n_fold, alpha, a, b, c, f, title, "/");
+    if save {
+        save_results(baseline, test, format!("{}_{}", title, "div").as_str());
+    }
 }
 
 fn const_select_template(
@@ -160,15 +187,19 @@ fn const_select_template(
     println!("H_1: {} != {}", x_str, u_str);
 
     let f = |cond0, cond1| {
-        (0..n_fold).fold(TpLnFixed::ONE, |_, _| {
-            black_box(F::select_from_4_f32(
-                black_box(cond0),
-                black_box(cond1),
-                black_box(1.0) * black_box(1e-38f32),
-                black_box(1.0) * black_box(1.0),
-                black_box(1.0) * black_box(1.0),
-                black_box(1.0) * black_box(1.0),
-            ))
+        black_box({
+            let mut z = F::ONE;
+            for _ in 0..n_fold {
+                z = black_box(F::select_from_4_f32(
+                    black_box(cond0),
+                    black_box(cond1),
+                    black_box(black_box(1.0) * black_box(1e-38f32)),
+                    black_box(black_box(1.0) * black_box(1.0)),
+                    black_box(black_box(1.0) * black_box(1.0)),
+                    black_box(black_box(1.0) * black_box(1.0)),
+                ));
+            }
+            z
         });
     };
 
@@ -213,29 +244,15 @@ fn time_measure_all<N>(
 ) -> (Vec<f64>, Vec<f64>) {
     let mut baseline = Vec::with_capacity(n_rounds);
     let mut test = Vec::with_capacity(n_rounds);
-    let mut choices = vec![false; n_rounds];
-    let mut rng = rand::thread_rng();
-    use rand::Fill;
-    choices.try_fill(&mut rng).unwrap();
     // warm up
-    for &c in &choices {
-        if c {
-            f_baseline();
-            f_test();
-        } else {
-            f_test();
-            f_baseline();
-        }
+    for _ in 0..n_rounds {
+        f_baseline();
+        f_test();
     }
 
-    for &c in &choices {
-        if c {
-            baseline.push(time_measure_single(&f_baseline) as f64);
-            test.push(time_measure_single(&f_test) as f64);
-        } else {
-            test.push(time_measure_single(&f_test) as f64);
-            baseline.push(time_measure_single(&f_baseline) as f64);
-        }
+    for _ in 0..n_rounds {
+        baseline.push(time_measure_single(&f_baseline) as f64);
+        test.push(time_measure_single(&f_test) as f64);
     }
     let baseline = baseline.into_iter().map(|v| v / n_fold as f64).collect();
     let test = test.into_iter().map(|v| v / n_fold as f64).collect();
@@ -285,7 +302,7 @@ fn ztest(
 fn denoise(v: Vec<f64>) -> Vec<f64> {
     let x = v.as_slice().mean();
     let s = v.as_slice().std_dev();
-    let k = 5.;
+    let k = 1.;
     v.into_iter()
         .filter(|&a| a < x + s * k && a > x - s * k)
         .collect()
